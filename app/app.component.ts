3a6e499ff11c9d4a7e834dfd0a9b552593fbc306
TaXxX <henrry_el_master@hotmail.com>
2016-11-19 01:38:28 -0400

    added pokemons

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,13 +4,13 @@ import { Component }          from '@angular/core';
     template: `
     <h1>{{title}}</h1>
     <nav>
-      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+      <a routerLink="/dashboard" routerLinkActive="active">Poké Dashboard</a>
+      <a routerLink="/heroes" routerLinkActive="active">Pokémons</a>
     </nav>
     <router-outlet></router-outlet>
   `,
     styleUrls: ['app/app.component.css']
 })
 export class AppComponent {
-    title = 'Tour of Heroes';
+    title = 'Pokédex';
 }
--- app/in-memory-data.service.ts
+++ app/in-memory-data.service.ts
@@ -2,16 +2,18 @@ import { InMemoryDbService } from 'angular2-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
     createDb() {
         let heroes = [
-            {id: 11, name: 'Mr. Nice'},
-            {id: 12, name: 'Narco'},
-            {id: 13, name: 'Bombasto'},
-            {id: 14, name: 'Celeritas'},
-            {id: 15, name: 'Magneta'},
-            {id: 16, name: 'RubberMan'},
-            {id: 17, name: 'Dynama'},
-            {id: 18, name: 'Dr IQ'},
-            {id: 19, name: 'Magma'},
-            {id: 20, name: 'Tornado'}
+            {id: 11, name: 'Bulbasaur'},
+            {id: 12, name: 'Ivysaur'},
+            {id: 13, name: 'Venusaur'},
+            {id: 14, name: 'Charmander'},
+            {id: 15, name: 'Charmeleon'},
+            {id: 16, name: 'Charizard'},
+            {id: 17, name: 'Squirtle'},
+            {id: 18, name: 'Wartortle'},
+            {id: 19, name: 'Blastoise'},
+            {id: 20, name: 'Caterpie'},
+            {id: 21, name: 'Metapod'},
+            {id: 22, name: 'Butterfree'}
         ];
         return {heroes};
     }
